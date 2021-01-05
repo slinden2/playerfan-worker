3b4ec11413ad69fff6794e758e721242b536fd3a
Samu Linden <samu.linden@gmail.com>
2021-01-05 17:32:36 +0100

    Adds required field to fetchDivisions

--- jobs/fetchDivisions.js
+++ jobs/fetchDivisions.js
@@ -2,6 +2,10 @@
  * This works for season 2019/2020. Divisions changed for 2020/2021 and API not yet updated
  */
 
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const { PrismaClient } = require("@prisma/client");
 const axios = require("axios");
 const divisions = require("./divisions.json");
@@ -21,19 +25,29 @@ const fetchDivisions = async () => {
   try {
     for (const division of divisions) {
       const divisionInDb = await prisma.division.findUnique({
-        where: { divisionId: division.divisionId },
+        where: {
+          divisionId_season: {
+            divisionId: division.divisionId,
+            season: process.env.SEASON,
+          },
+        },
       });
       if (divisionInDb) continue;
 
       const conferenceId = ConferenceDivMap[division.shortName];
 
       const newDivision = {
+        season: process.env.SEASON,
         divisionId: division.divisionId,
         link: division.link,
         name: division.name,
         shortName: division.shortName,
         abbreviation: division.abbreviation,
-        conference: { connect: { conferenceId } },
+        conference: {
+          connect: {
+            conferenceId_season: { conferenceId, season: process.env.SEASON },
+          },
+        },
         active: division.active,
       };
       await prisma.division.create({
